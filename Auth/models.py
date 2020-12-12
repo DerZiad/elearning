ee2976f4cb81cbfe272d3646ea100a503d812150
Ziad <ziadbougrine@gmail.com>
2020-12-12 16:31:50 +0100

    Hey team , i added Person and Professor models.py , import it in your models and work with it

--- Auth/models.py
+++ Auth/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+   nom = models.CharField(max_length=20)
+   prenom = models.CharField(max_length=20)
+   datedenaisse = models.DateField
+   username = models.CharField(max_length=20)
+   #Professor
+   email = models.EmailField(max_length=40)
+   password = models.CharField(max_length=50)
+   datecreationaccount = models.DateField(auto_created=True)
+
+class Professor(Person):
+   pass
