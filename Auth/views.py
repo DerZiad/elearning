5c0f2747ede61ba32cc61377beefeacd9c7780a0
Ziad <ziadbougrine@gmail.com>
2020-12-12 16:52:59 +0100

    Hey that is my html login

--- Auth/views.py
+++ Auth/views.py
@@ -4,5 +4,14 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    template = loader.get_template("Auth/index.html")
+    template = loader.get_template("Auth/connection.html")
     return HttpResponse(template.render(request = request))
+
+
+def inscription(request):
+    template = loader.get_template("Connection/inscription.html")
+    return HttpResponse(template.render(request = request))
+
+def seconnecter(request):
+    template = loader.get_template("Connection/connection.html")
+    return HttpResponse(template.render(request= request))
