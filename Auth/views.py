e15260510e28cc44564f11c40c1545772c95e616
Ziad <ziadbougrine@gmail.com>
2020-12-12 12:37:25 +0100

    manage.py edited

--- Auth/views.py
+++ Auth/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+
+def index(request):
+    template = loader.get_template("Auth/index.html")
+    return HttpResponse("szi")
--- germanstudies/urls.py
+++ germanstudies/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Auth import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Auth',urls)
 ]
