471aff0fc32b81a951d89c41ebaa612901c15272
smookerzz <z>
2020-12-19 20:03:30 +0100

    all

--- Auth/views.py
+++ Auth/views.py
@@ -48,5 +48,5 @@ def seconnecter(request):
         else:
             return JsonResponse()
     else:
-        template = loader.get_template("login/login.html")
+        template = loader.get_template("login/signin.html")
         return HttpResponse(template.render(request= request))
