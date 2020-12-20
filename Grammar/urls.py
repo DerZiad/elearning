8dc29ceb7cd48114292d43be4201f288692b9cbe
ABOUHALI <abouhaliayman@gmail.com>
2020-12-20 12:29:15 +0100

    tkhrbiqat

--- Grammar/urls.py
+++ Grammar/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
-
+app_name="Grammar"
 urlpatterns =[
     url(r'^$', views.grammarex),
+
 ]
--- Grammar/views.py
+++ Grammar/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 from Grammar.models import Ubung
 
 
 def grammarex(request):
-    questions = Ubung.objects
-    context={
-        'questions':questions,
-    }
-    return render(request,'Grammar/test.html',context)
+    questions = Ubung.objects.filter(losung=3)
+    msg=["<li>{}</li>".format(beispiel.title) for beispiel in questions]
+    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
+
+    return HttpResponse()
