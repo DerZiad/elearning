7df4a123ecc402be08f3d5bcff15b91cd16a9882
ABOUHALI <abouhaliayman@gmail.com>
2020-12-20 12:39:33 +0100

    tkhrbiqat

--- Grammar/views.py
+++ Grammar/views.py
@@ -7,7 +7,7 @@ from Grammar.models import Ubung
 
 def grammarex(request):
     questions = Ubung.objects.filter(losung=3)
-    msg=["<li>{}</li>".format(beispiel.title) for beispiel in questions]
-    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
-
-    return HttpResponse()
+    msg=["<li>{}</li>".format(beispiel.frage) for beispiel in questions]
+    message = """<ul>{}</ul>""".format("\n".join(msg))
+    template = loader.get_template('Grammar/base.html')
+    return HttpResponse(template.render(request=request))
