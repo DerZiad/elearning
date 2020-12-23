b4c18c6b709956d3c680ca27dbee567b33818775
ABOUHALI <abouhaliayman@gmail.com>
2020-12-23 09:41:15 +0100

    ayman

--- Grammar/models.py
+++ Grammar/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class Ubung(models.Model):
-    frage = models.CharField(max_length=500)
+    frage = models.CharField(max_length=200)
     losung = models.IntegerField()
+
+
     def __str__(self):
         return self.frage
--- Grammar/views.py
+++ Grammar/views.py
@@ -3,16 +3,33 @@ from builtins import print
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from . import models
 
 from Grammar.models import Ubung
 
 
 def grammarex(request):
-    questions = Ubung.objects
-    losung = request.GET.get('losung')
-    context={
-        'questions':questions
-    }
-    if losung == "wo":
-         print("gut")
-    return render(request, 'Grammar/index.html', context)
+
+    ubungs = Ubung.objects.all()
+
+    if(request.method == "POST"):
+        losung = request.POST
+        cmp = 0
+        context = {
+            'ubungs': ubungs
+        }
+        for ubung in ubungs:
+            print(ubung)
+            print("ziad",losung[ubung.frage])
+            print(ubung.losung)
+            if str(losung[ubung.frage]) == str(ubung.losung):
+                cmp += 1
+        print("le nombre de question acuis",cmp)
+        return render(request, 'Grammar/index.html', context)
+    else:
+        losung = request.GET
+
+        context = {
+            'ubungs': ubungs
+        }
+        return render(request, 'Grammar/index.html', context)
