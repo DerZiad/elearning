6942b028610a631fa3109d2476219e5e87b42c26
yqsser <yasserbbbbb>
2020-12-26 12:30:59 +0100

    Dakchi khfif drif

--- Horen/urls.py
+++ Horen/urls.py
@@ -1,2 +1,11 @@
+from django.urls import path
+from . import views
+
 app_name = "Horen"
-urlpatterns = []
+urlpatterns = [
+    path('', views.Horen , name='Horen'),
+    path('modeltest1/', views.modeltest1 , name='ModelTest1'),
+    path('modeltest2/', views.modeltest2 , name='ModelTest2'),
+    path('modeltest3/', views.modeltest3 , name='ModelTest3'),
+    path('poadcast/', views.poadcast, name='Poadcast'),
+]
--- Horen/views.py
+++ Horen/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def Horen(request):
+    return render(request, 'Horen/base.html')
+
+
+def modeltest1(request):
+    return render(request, 'Horen/modeltest1.html')
+
+
+def modeltest2(response):
+    return HttpResponse('<h1> ModelTest 1</h1>')
+
+
+def modeltest3(response):
+    return HttpResponse('<h1> ModelTest 1</h1>')
+
+
+def poadcast(response):
+    return HttpResponse('<h1>Poadcast</h1>')
