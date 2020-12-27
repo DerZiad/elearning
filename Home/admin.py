2588cf47d6e4c6769f08e591cc770b344b8a6f69
yqsser <yasserbbbbb>
2020-12-27 17:26:36 +0100

    admin

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from Horen.models import ModelTest,Track
+from Auth.models import Personne, Professeur
+from Grammar.models import Ubung, Essai
 
-# Register your models here.
+admin.site.register(ModelTest)
+admin.site.register(Track)
+admin.site.register(Personne)
+admin.site.register(Professeur)
+admin.site.register(Ubung)
+admin.site.register(Essai)
