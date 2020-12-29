572705c456122c446c4669907c611021218432be
MOURAD_PC <fxxcvxcvxcv>
2020-12-29 11:04:23 +0100

    hhhhh

--- Schreiben/models.py
+++ Schreiben/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class text(models.Model):
+    titre = models.CharField(max_length=200, unique=True)
+    corps = models.CharField(max_length=300)
+
+
+class Reponse(models.Model):
+    rep = models.TextField(blank=True, null=True)
+    #txt = models.ForeignKey(text, on_delete=models.CASCADE)
+
+
