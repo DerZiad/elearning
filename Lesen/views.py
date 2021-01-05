8fe3653e4f958775c1504e14a94e585c0432bcc1
bouvhakur <amine@gmail.com>
2021-01-05 11:07:45 +0100

    hi

--- Lesen/views.py
+++ Lesen/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 
-def listing (request):
-    return render(request, 'Lesen/ubungs.html')
+from Lesen.models import Text, Fragen, Answers
+
+
+def listing(request):
+    if request.method == "GET":
+        texts = Text.objects.all()
+        fragen = Fragen.objects.all()
+        answers = Answers.objects.all()
+
+        dic = {
+        }
+        list = []
+        for texte in texts:
+            list.append(texte.text)
+            for frag in fragen:
+
+                if frag.numtext == texte:
+                    list.append(frag.frage)
+                    for ans in answers:
+                        if ans.numfra == frag:
+                            list.append(ans.an)
+                list = []
+                dic[str(texte.title)] = list
+
+    context = {
+        'dictionnaire': dic
+    }
+    return render(request, 'Lesen/ubungs.html', context)
 
 # Create your views here.
