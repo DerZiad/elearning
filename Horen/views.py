2382ba8e8bc6a5f038a6d901d2a644fd8679998b
yqsser <yasserbbbbb>
2021-01-09 21:24:39 +0100

    dqsdqsqds

--- Horen/views.py
+++ Horen/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from Home.funktions import funktion as sv
 from django.shortcuts import render
+from . import randomer as random
 from Auth.models import Personne
-#import spotipy
-#from spotipy.oauth2 import SpotifyClientCredentials
+from .models import *
+
 
 
 def Horen(request):
@@ -10,9 +12,25 @@ def Horen(request):
 
 
 def modeltest1(request):
-    personne = Personne.objects.filter(username=request.session['Username'])
-    request.session['succes_horen']
-    return render(request, 'Horen/modeltest1.html')
+        #personne = Personne.objects.filter(username=request.session['Username'])
+        #request.session['succes_horen']
+        questions = Question.objects.filter(audio__modeltest_id=1)
+        dic = {}
+        dictrack = {}
+        for question in questions:
+            list = []
+            choix = Choix.objects.filter(question = question)
+            for ch in choix:
+                list.append(ch)
+            list.insert(random.generateRandom(),question.reponse)
+            dic[question.quest] = list
+            dictrack[question.quest] = question.audio
+
+        context = {
+            'audios':dictrack,
+            'questions':dic,
+        }
+        return render(request, 'Horen/modeltest1.html', context)
 
 
 def modeltest2(request):
