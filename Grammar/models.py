85e636c19d5564e29d99802f953dc50257a56c9a
ABOUHALI <abouhaliayman@gmail.com>
2021-01-10 19:01:04 +0100

    ziad

--- Grammar/models.py
+++ Grammar/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from Auth.models import Personne
+
 
 class Ubung(models.Model):
     frage = models.CharField(max_length=200)
@@ -7,7 +9,10 @@ class Ubung(models.Model):
     type = models.CharField(max_length=200)
     def __str__(self):
         return self.frage
-
+class Reponse(models.Model):
+    ubung = models.OneToOneField(Ubung, on_delete=models.CASCADE)
+    pers= models.ForeignKey(Personne, on_delete=models.CASCADE,related_name='pers')
+    valide = models.BooleanField(default=False)
 
 class Essai(models.Model):
     choix = models.CharField(max_length=15)
